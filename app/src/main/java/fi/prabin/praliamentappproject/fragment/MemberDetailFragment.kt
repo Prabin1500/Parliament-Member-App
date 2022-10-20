4c8326f34c8d5696666a7ca25170654d5e28d7ae
prabind <prabin.dhakal@metropolia.fi>
2022-10-20 15:52:24 +0300

    created view model and view model factory class for detail fragment.

--- app/src/main/java/fi/prabin/praliamentappproject/fragment/MemberDetailFragment.kt
+++ app/src/main/java/fi/prabin/praliamentappproject/fragment/MemberDetailFragment.kt
@@ -6,21 +6,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import fi.prabin.praliamentappproject.R
+import fi.prabin.praliamentappproject.databinding.FragmentMemberDetailBinding
+import fi.prabin.praliamentappproject.viewmodel.FragmentMemberDetailViewModel
 
-
+/**
+* Fragment which stores the detail information of the members.
+*/
 class MemberDetailFragment : Fragment() {
+
+    private var _binding: FragmentMemberDetailBinding? = null
+    private val binding get() = _binding!!
+
+    var hetekaId : Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //Get the hetekaId's value passed from Member List Fragment
+        hetekaId = arguments?.getInt("hetekaID")
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_member_detail, container, false)
+        _binding = FragmentMemberDetailBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 
--- app/src/main/java/fi/prabin/praliamentappproject/viewmodel/FragmentMemberDetailViewModel.kt
+++ app/src/main/java/fi/prabin/praliamentappproject/viewmodel/FragmentMemberDetailViewModel.kt
@@ -1,4 +1,52 @@
 package fi.prabin.praliamentappproject.viewmodel
 
-class FragmentMemberDetailViewModel {
+import android.app.Application
+import androidx.lifecycle.*
+import fi.prabin.praliamentappproject.database.ParliamentMemberExtraInfo
+import fi.prabin.praliamentappproject.database.ParliamentMemberInfo
+import fi.prabin.praliamentappproject.database.ParliamentMemberInfoRepository
+import fi.prabin.praliamentappproject.database.ParliamentMemberRoomDatabase
+import kotlinx.coroutines.launch
+
+class FragmentMemberDetailViewModel(application: Application) : AndroidViewModel(application) {
+
+    //All the listed members
+    private val _member= MutableLiveData<List<ParliamentMemberInfo>>()
+    val member: LiveData<List<ParliamentMemberInfo>> = _member
+
+    private val _extraInfo = MutableLiveData<List<ParliamentMemberExtraInfo>>()
+    val extraInfo : LiveData<List<ParliamentMemberExtraInfo>> = _extraInfo
+
+    //Initialize the repository
+    private val parliamentMemberInfoRepository = ParliamentMemberInfoRepository(
+        ParliamentMemberRoomDatabase.getDatabase(application).parliamentMemberInfoDao(),
+        ParliamentMemberRoomDatabase.getDatabase(application).parliamentMemberExtraInfoDao()
+    )
+
+    //Get all members data from the Room Database
+    fun getAllMemberInfo(){
+        viewModelScope.launch {
+            _member.value = parliamentMemberInfoRepository.readAllMemberInfo
+        }
+    }
+
+    /**
+     * Launching a new coroutine to get the data in a non-blocking way
+     */
+    fun getAllExtraInfo(){
+        viewModelScope.launch {
+            _extraInfo.value = parliamentMemberInfoRepository.readAllExtraInfo
+        }
+    }
+}
+
+//Factory class for DetailViewModel class
+class MemberDetailViewModelFactory(val application: Application) : ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(FragmentMemberDetailViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return FragmentMemberDetailViewModel(application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
